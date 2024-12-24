4dd71ba442ef6b70c90ec8fe298625e038b148d5
aktky <aktky4d@gmail.com>
2024-12-24 22:40:47 +0900

    fix

--- runtime.go
+++ runtime.go
@@ -1,6 +1,9 @@
 package sourcetool
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"log"
 	"sync"
 	"time"
@@ -45,10 +48,9 @@ func startRuntime(apiKey, endpoint string, pages map[uuid.UUID]*page) *runtime {
 		}
 
 		r.wsClient = wsClient
-		msgHandler := &messageHandler{r}
-		wsClient.RegisterHandler(websocket.MessageMethodInitializeClient, msgHandler.initializeCilent)
-		wsClient.RegisterHandler(websocket.MessageMethodRerunPage, msgHandler.rerunPage)
-		wsClient.RegisterHandler(websocket.MessageMethodCloseSession, msgHandler.closeSession)
+		wsClient.RegisterHandler(websocket.MessageMethodInitializeClient, r.initializeCilent)
+		wsClient.RegisterHandler(websocket.MessageMethodRerunPage, r.rerunPage)
+		wsClient.RegisterHandler(websocket.MessageMethodCloseSession, r.closeSession)
 
 		r.initializeHost(apiKey, pages)
 	})
@@ -79,4 +81,106 @@ func (r *runtime) initializeHost(apiKey string, pages map[uuid.UUID]*page) {
 	}
 
 	log.Printf("initialize host message sent: %v", resp)
+}
+
+func (r *runtime) initializeCilent(msg *websocket.Message) error {
+	var p websocket.InitializeClientPayload
+	if err := json.Unmarshal(msg.Payload, &p); err != nil {
+		return fmt.Errorf("failed to unmarshal payload: %v", err)
+	}
+
+	sessionID, err := uuid.FromString(p.SessionID)
+	if err != nil {
+		return err
+	}
+	pageID, err := uuid.FromString(p.PageID)
+	if err != nil {
+		return err
+	}
+
+	log.Println("Creating new session with ID:", sessionID)
+	session := session.New(sessionID)
+	r.sessionManager.SetSession(session)
+
+	page := r.pageManager.getPage(pageID)
+	if page == nil {
+		return fmt.Errorf("page not found: %s", pageID)
+	}
+
+	ui := &uiBuilder{
+		context: context.Background(),
+		runtime: r,
+		session: session,
+		page:    page,
+		cursor:  newCursor(main),
+	}
+
+	if err := page.run(ui); err != nil {
+		return fmt.Errorf("failed to run page: %v", err)
+	}
+
+	return nil
+}
+
+func (r *runtime) rerunPage(msg *websocket.Message) error {
+	var p websocket.RerunPagePayload
+	if err := json.Unmarshal(msg.Payload, &p); err != nil {
+		return fmt.Errorf("failed to unmarshal payload: %v", err)
+	}
+
+	sessionID, err := uuid.FromString(p.SessionID)
+	if err != nil {
+		return err
+	}
+	sess := r.sessionManager.GetSession(sessionID)
+	if sess == nil {
+		return fmt.Errorf("session not found: %s", sessionID)
+	}
+
+	pageID, err := uuid.FromString(p.PageID)
+	if err != nil {
+		return err
+	}
+	page := r.pageManager.getPage(pageID)
+	if page == nil {
+		return fmt.Errorf("page not found: %s", pageID)
+	}
+
+	var states map[uuid.UUID]any
+	if err := json.Unmarshal(p.State, &states); err != nil {
+		return fmt.Errorf("failed to unmarshal state: %v", err)
+	}
+	sess.State.SetStates(states)
+
+	ui := &uiBuilder{
+		context: context.Background(),
+		runtime: r,
+		session: sess,
+		page:    page,
+		cursor:  newCursor(main),
+	}
+
+	if err := page.run(ui); err != nil {
+		return fmt.Errorf("failed to run page: %v", err)
+	}
+
+	sess.State.ResetButtons()
+
+	return nil
+}
+
+func (r *runtime) closeSession(msg *websocket.Message) error {
+	var p websocket.CloseSessionPayload
+	if err := json.Unmarshal(msg.Payload, &p); err != nil {
+		return fmt.Errorf("failed to unmarshal payload: %v", err)
+	}
+
+	sessionID, err := uuid.FromString(p.SessionID)
+	if err != nil {
+		return err
+	}
+
+	r.sessionManager.DeleteSession(sessionID)
+
+	return nil
 }
--- sourcetool.go
+++ sourcetool.go
@@ -42,3 +42,72 @@ func (s *Sourcetool) Listen() error {
 func (s *Sourcetool) Close() error {
 	return s.runtime.wsClient.Close()
 }
+
+type page struct {
+	id      uuid.UUID
+	name    string
+	handler func(UIBuilder) error
+}
+
+func (p *page) run(ui UIBuilder) error {
+	if err := p.handler(ui); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Sourcetool) Page(name string, handler func(UIBuilder) error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	p := &page{
+		id:      s.generatePageID(name),
+		name:    name,
+		handler: handler,
+	}
+
+	s.pages[p.id] = p
+	if len(s.navigations) > 0 {
+		currentNav := s.navigations[len(s.navigations)-1]
+		currentNav.pages = append(currentNav.pages, p)
+	}
+}
+
+func (s *Sourcetool) generatePageID(pageName string) uuid.UUID {
+	ns := uuid.NewV5(uuid.NamespaceDNS, fmt.Sprintf("%s.trysourcetool.com", s.subdomain))
+	return uuid.NewV5(ns, pageName)
+}
+
+type pageManager struct {
+	pages map[uuid.UUID]*page
+	mu    sync.RWMutex
+}
+
+func newPageManager(pages map[uuid.UUID]*page) *pageManager {
+	return &pageManager{
+		pages: pages,
+	}
+}
+
+func (s *pageManager) getPage(id uuid.UUID) *page {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return s.pages[id]
+}
+
+type navigation struct {
+	name  string
+	pages []*page
+}
+
+func (s *Sourcetool) Navigation(name string, handler func()) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	currentNav := &navigation{
+		name: name,
+	}
+	s.navigations = append(s.navigations, currentNav)
+
+	handler()
+}
